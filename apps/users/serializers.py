c8a1332db31b99e9af1beeaa16e12136c2b1841d
Juan David Torres <juan.torres.canon@correounivalle.edu.co>
2020-11-13 23:04:28 -0500

    Modify users

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -7,3 +7,10 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'email', 'password', 'id_card', 'role', 'phone', 'birthdate')
         extra_kwargs = {'password': {'write_only': True, 'min_length': 6}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
