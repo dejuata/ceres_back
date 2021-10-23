cbf952105d6d722e6c71bc54448e2b3da52bad62
Juan David Pino <dejuata@hotmail.com>
2021-10-23 08:38:08 -0500

    Deploy

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
 release: python manage.py makemigrations --no-input
 release: python manage.py migrate --no-input
 
-web: gunicorn ceres_back.wsgi
+web: gunicorn config.wsgi
--- config/asgi.py
+++ config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for freeCodeSchoolAPI project.
+ASGI config for Ceres project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
