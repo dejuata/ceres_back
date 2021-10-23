93d13610dc1f5b47b225186f300ff717baeff054
Juan David Pino <dejuata@hotmail.com>
2021-10-23 07:51:17 -0500

    redocs

--- apps/docs/urls.py
+++ apps/docs/urls.py
@@ -1,22 +1,31 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from rest_framework.schemas import get_schema_view
+# from rest_framework.schemas import get_schema_view
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny
+from rest_framework import permissions
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
-@permission_classes([AllowAny])
-def schemaView():
-    return TemplateView.as_view(
-        template_name='redoc.html',
-        extra_context={'schema_url': 'openapi-schema'}
-    )
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Ceres App",
+        default_version='v1',
+        description="API PWA",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="juan.david.pino.reyes@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('', schemaView(), name='redoc'),
-    path('openapi', get_schema_view(
-        title="Ceres App",
-        description="API PWA",
-        version="1.0.0"),
-        name='openapi-schema'
-    )
+    path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ]
+# path('openapi', get_schema_view(
+#         title="Ceres App",
+#         description="API PWA",
+#         version="1.0.0"),
+#         name='openapi-schema'
+#     )
--- config/urls.py
+++ config/urls.py
@@ -4,8 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('apps.docs.urls')),
     path('admin/', admin.site.urls),
-    path('api/docs/', include('apps.docs.urls')),
     path('api/auth/', include('apps.authentication.urls')),
     path('api/users/', include('apps.users.urls')),
     path('api/zones/', include('apps.zones.urls')),
